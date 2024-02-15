4453624f0168025557df7f3f9c02fdf001da62bd
Justine Fidel <justine1410@live.fr>
2024-02-15 14:58:21 +0100

    ajout d'un textarea

--- histoires.js
+++ histoires.js
@@ -2,8 +2,7 @@ const histoires = {
   1:` 
   Il était une fois, dans un monde ancien où les dinosaures régnaient en maîtres, un petit dinosaure nommé Rex. 
   Il était différent des autres dinosaures : plus petit, mais plus agile et plus malin. Un jour, alors qu'il explorait la forêt préhistorique, 
-  il découvrit un mystérieux objet brillant sous un buisson. C'était une étrange boîte en pierre contenant une carte avec des symboles énigmatiques. I
-  ntrigué, Rex décida de suivre cette carte pleine de promesses. Il traversa des jungles touffues, escalada des falaises escarpées et évita des pièges sournois. 
+  il découvrit un mystérieux objet brillant sous un buisson. C'était une étrange boîte en pierre contenant une carte avec des symboles énigmatiques. Intrigué, Rex décida de suivre cette carte pleine de promesses. Il traversa des jungles touffues, escalada des falaises escarpées et évita des pièges sournois. 
   Finalement, après de nombreuses aventures, Rex découvrit un trésor inestimable : un nid rempli de délicieux œufs de dinosaures multicolores. Heureux, il rentra chez lui, 
   convaincu qu'il n'avait pas besoin d'un trésor pour être le plus grand explorateur de tous les temps. Car après tout, avec son courage et sa détermination, il 
   pouvait conquérir n'importe quel défi et vivre des aventures extraordinaires, même dans un monde de dinosaures. Et c'est ainsi que le petit Rex comprit que le 
@@ -15,6 +14,9 @@ const histoires = {
   Il dansa avec le géant sous la lueur de la lune, lui montrant l'amitié. Touché, le géant abandonna ses plans. Dorénavant, Bulbao et le géant étaient amis, diffusant joie et bonheur. 
   La vallée célébrait leur amitié, prouvant que même les monstres peuvent être gentils. Et Bulbao, le petit monstre vert, illuminait leur vie.
     `,
+
+    3:`Dans une vallée verdoyante`,
+  
     // Ajoutez d'autres histoires ici en associant chaque numéro d'option à son contenu
   };
   
--- index.js
+++ index.js
@@ -1,51 +1,57 @@
-                                                
-                                                /*******les histoires******************/
-  const histoires = {
-    1:` 
-    Il était une fois, dans un monde ancien où les dinosaures régnaient en maîtres, un petit dinosaure nommé Rex. 
-    Il était différent des autres dinosaures : plus petit, mais plus agile et plus malin. Un jour, alors qu'il explorait la forêt préhistorique, 
-    il découvrit un mystérieux objet brillant sous un buisson. C'était une étrange boîte en pierre contenant une carte avec des symboles énigmatiques. I
-    ntrigué, Rex décida de suivre cette carte pleine de promesses. Il traversa des jungles touffues, escalada des falaises escarpées et évita des pièges sournois. 
-    Finalement, après de nombreuses aventures, Rex découvrit un trésor inestimable : un nid rempli de délicieux œufs de dinosaures multicolores. Heureux, il rentra chez lui, 
-    convaincu qu'il n'avait pas besoin d'un trésor pour être le plus grand explorateur de tous les temps. Car après tout, avec son courage et sa détermination, il 
-    pouvait conquérir n'importe quel défi et vivre des aventures extraordinaires, même dans un monde de dinosaures. Et c'est ainsi que le petit Rex comprit que le 
-    plus grand trésor était l'amitié et les merveilleuses aventures partagées avec ses compagnons dinosaures.`,
-  
-      2:`
-    Dans une vallée verdoyante, vivait Bulbao, un petit monstre vert. Contrairement à ses semblables, Bulbao préférait les fleurs aux frayeurs. 
-    Un jour, un géant effrayant menaça le village. Intrépide, Bulbao s'avança. Il découvrit que le géant était solitaire et triste. Bulbao eut une idée brillante. 
-    Il dansa avec le géant sous la lueur de la lune, lui montrant l'amitié. Touché, le géant abandonna ses plans. Dorénavant, Bulbao et le géant étaient amis, diffusant joie et bonheur. 
-    La vallée célébrait leur amitié, prouvant que même les monstres peuvent être gentils. Et Bulbao, le petit monstre vert, illuminait leur vie.
-      `,
-      // Ajoutez d'autres histoires ici en associant chaque numéro d'option à son contenu
-    };
-                                                
-console.log('coucou');
 
-// Sélection de l'élément select
+                               
+                                                /*******les histoires******************/
+
+                                                
+// Importez les histoires depuis le fichier histoires.js
+import histoires from './histoires.js';
+
+// Sélection de l'élément select pour l'histoire
 const selectHistoire = document.getElementById('selectHistoire');
 
+// Sélection de l'élément select pour la typographie
+const selectTypo = document.getElementById('selectTypo');
+
 // Sélection de l'élément texte où afficher l'histoire
-const texteHistoire = document.querySelector('.texteHistoire');
+let texteHistoire = document.querySelector('.texteHistoire');
+
+                                                /*******Ecriture du texte souhaitez******************/
+
+ // Récupérer le textarea et la div
+ var textarea = document.getElementById("inputText");
+
+ // Ajouter un écouteur d'événement sur le changement de contenu du textarea
+ textarea.addEventListener("input", function() {
+     // Mettre à jour le contenu de la div avec le contenu du textarea
+     texteHistoire.textContent = "";
+     texteHistoire.textContent = textarea.value;
+ });
+
 
 // Fonction pour afficher l'histoire sélectionnée
 function afficherHistoire() {
-  const histoireSelectionnee = selectHistoire.value;
-  const histoire = histoires[histoireSelectionnee];
-  const histoireAvecCouleur = colorierLettresMuette(histoire);
-  texteHistoire.innerHTML = histoireAvecCouleur;
+    const histoireSelectionnee = selectHistoire.value;
+    const histoire = histoires[histoireSelectionnee];
+    texteHistoire.innerHTML = histoire;
 }
 
-// Ajout d'un écouteur d'événement pour détecter les changements de sélection
+// Ajout d'un écouteur d'événement pour détecter les changements de sélection de l'histoire
 selectHistoire.addEventListener('change', afficherHistoire);
 
-// Fonction pour mettre en couleur les lettres muettes
-function colorierLettresMuette(mot) {
-    return mot.replace(/[^aeiouy\s]/gi, '<span class="muette">$&</span>');
-  }
-  
+// Ajout d'un écouteur d'événement pour détecter les changements de sélection de la typographie
+selectTypo.addEventListener('change', function() {
+    // Récupérer la valeur de la typographie sélectionnée
+    var selectedTypo = selectTypo.value;
+
+    // Appliquer la typographie au texte de l'histoire
+    texteHistoire.style.fontFamily = selectedTypo;
+});
+
 // Affichage initial de l'histoire sélectionnée
 afficherHistoire();
+
+
+
 
 
 
@@ -60,17 +66,22 @@ let button = document.querySelector(".go")
 button.addEventListener('click',()=>{
 
             // Sélection de la div contenant le texte
-        const texteKaraoke = document.querySelector('.texteHistoire');
+        // const texteHistoire = document.querySelector('.texteHistoire');
 
         // Récupération de chaque mot
-        const mots = texteKaraoke.textContent.split(' ');
+        const mots = texteHistoire.textContent.split(' ');
 
         // Remplacement du contenu de la div par des spans autour de chaque mot
-        texteKaraoke.innerHTML = mots.map(mot => `<span class="mot">${mot}</span>`).join(' ');
+        texteHistoire.innerHTML = mots.map(mot => `<span class="mot">${mot}</span>`).join(' ');
 
         // Sélection de tous les mots
         const motsElements = document.querySelectorAll('.mot');
-
+        // Fonction pour réinitialiser le texte après la fin de l'animation karaoké
+          function reinitialiserTexte() {
+            motsElements.forEach(mot => {
+              mot.classList.remove('en-vedette'); // Supprimer la classe pour mettre en évidence le mot
+            });
+          }
         // Fonction pour mettre en évidence chaque mot progressivement
         function mettreEnVedette() {
         motsElements.forEach((mot, index) => {
@@ -88,12 +99,6 @@ button.addEventListener('click',()=>{
     mettreEnVedette()
 })
 
-// Fonction pour réinitialiser le texte après la fin de l'animation karaoké
-function reinitialiserTexte() {
-  motsElements.forEach(mot => {
-    mot.classList.remove('en-vedette'); // Supprimer la classe pour mettre en évidence le mot
-  });
-}
 
 
 
